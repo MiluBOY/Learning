345a55568dd78e3acdaf51839a1a98dd3b46c11e
YingLuoHuaFa <2859624462@qq.com>
2023-02-27 14:12:38 +0800

    YingLuoHuaFa

---
+++ print.py
@@ -0,0 +1,15 @@
+#DAY1
+print("Dad!!")
+print('你好!'+'我是'+'YingLuoHuaFa。')
+print('Let\'go')
+print('你好呀\n'+'哈哈哈')
+print('''我是第一行
+我是第二行''')
+##DAY2
+greet = "你好，吃了吗，"
+greet_chinese = greet
+greet_english = "Yo what's up,"
+greet = greet_english
+print(greet + "张三")
+print(greet + "李四")
+print(greet_chinese)
