56691a964cd74dd126a2ae564a60b912f05aa98b
YingLuoHuaFa <2859624462@qq.com>
2023-03-14 11:43:08 +0800

    YingLuoHuaFa

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,25 +1,27 @@
+import os
 import tkinter as tk
+from tkinter import filedialog
+from pygame import mixer
+
+mixer.init()  # 初始化音频
 
 # 创建窗口
 window = tk.Tk()
-window.title('交互面板')
+window.title('音乐播放器')
 
 # 添加标签
-label = tk.Label(window, text='请输入一个数值')
+label = tk.Label(window, text='请选择要播放的音乐文件')
 label.pack()
 
-# 添加输入框
-entry = tk.Entry(window)
-entry.pack()
-
-# 定义计算函数
-def calculate():
-    value = float(entry.get())
-    result = value ** 2
-    label.config(text='结果：{}'.format(result))
+# 定义函数：选择音乐文件
+def choose_file():
+    file_path = filedialog.askopenfilename()
+    mixer.music.load(file_path)
+    label.config(text='正在播放：{}'.format(os.path.basename(file_path)))
+    mixer.music.play()
 
 # 添加按钮
-button = tk.Button(window, text='计算', command=calculate)
+button = tk.Button(window, text='选择文件', command=choose_file)
 button.pack()
 
 # 运行程序
