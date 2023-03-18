468179b160369d4e25616bb8cb091fc0f9b5a4aa
YingLuoHuaFa <2859624462@qq.com>
2023-03-18 22:02:34 +0800

    YingLuoHuaFa

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,43 +1,91 @@
-import time
+import random
+import pygame
 
-# 开场白
-print("欢迎来到迷失森林！你已经迷失在这个森林里很久了，现在需要尽快找到出口才能逃脱。")
+# 初始化 pygame
+pygame.init()
 
-# 定义游戏场景和可行操作
-scenes = {
-    "开始": {
-        "描述": "你站在一个草丛中，周围是茂密的树林。你看到两个方向可以前进，一个是往东，一个是往南。",
-        "可行操作": ["向东走", "向南走"]
-    },
-    "往东走": {
-        "描述": "你走进了树林深处，看到一只熊在远处出没。你需要小心避开。",
-        "可行操作": ["返回开始", "继续向前"]
-    },
-    "向南走": {
-        "描述": "你走过了一片开阔地，发现一条小溪流过。你需要决定是跟着溪流走还是离开它。",
-        "可行操作": ["跟着溪流走", "离开溪流"]
-    },
-    "跟着溪流走": {
-        "描述": "你继续跟着溪流走，一直走到了一个瀑布前。你可以试着爬上去看看。",
-        "可行操作": ["返回南边", "爬上瀑布"]
-    },
-    "离开溪流": {
-        "描述": "你离开了溪流，绕过一片危险的沼泽地。现在你来到了一条小路上，可以继续往前走。",
-        "可行操作": ["返回南边", "继续前进"]
-    },
-    "爬上瀑布": {
-        "描述": "你成功爬上了瀑布，但你发现前面是一片荒凉的沙漠，无路可走。你只能返回。",
-        "可行操作": ["返回瀑布前"]
-    },
-    "返回开始": {
-        "描述": "你返回了草丛中。",
-        "可行操作": ["向东走", "向南走"]
-    },
-    "返回南边": {
-        "描述": "你返回了南边。",
-        "可行操作": ["跟着溪流走", "离开溪流"]
-    },
-    "继续前进": {
-        "描述": "你走过了一座小山，发现前方有一座古老的城堡。你可以试着进去看看。",
-        "可行操作": ["返回路径", "进入城堡"]
-    },}
+# 定义颜色
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# 定义屏幕大小和格子大小
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+CELL_SIZE = 20
+
+# 创建屏幕对象
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+# 设置窗口标题
+pygame.display.set_caption("贪吃蛇游戏")
+
+# 定义贪吃蛇类
+class Snake:
+    def __init__(self):
+        # 初始位置
+        self.x = SCREEN_WIDTH // 2
+        self.y = SCREEN_HEIGHT // 2
+        # 初始速度和方向
+        self.speed = CELL_SIZE
+        self.direction = "right"
+        # 初始身体长度和颜色
+        self.body = [(self.x, self.y)]
+        self.color = GREEN
+
+    # 更新贪吃蛇位置和方向
+    def update(self):
+        # 根据方向更新位置
+        if self.direction == "right":
+            self.x += self.speed
+        elif self.direction == "left":
+            self.x -= self.speed
+        elif self.direction == "up":
+            self.y -= self.speed
+        elif self.direction == "down":
+            self.y += self.speed
+        # 更新身体
+        self.body.insert(0, (self.x, self.y))
+        if len(self.body) > 1:
+            self.body.pop()
+
+    # 改变方向
+    def change_direction(self, direction):
+        if direction == "right" and self.direction != "left":
+            self.direction = "right"
+        elif direction == "left" and self.direction != "right":
+            self.direction = "left"
+        elif direction == "up" and self.direction != "down":
+            self.direction = "up"
+        elif direction == "down" and self.direction != "up":
+            self.direction = "down"
+
+    # 判断是否吃到食物
+    def eat_food(self, food):
+        if self.x == food.x and self.y == food.y:
+            self.body.append((self.x, self.y))
+            return True
+        else:
+            return False
+
+    # 判断是否碰到边界或身体
+    def check_collision(self):
+        if self.x < 0 or self.x > SCREEN_WIDTH - CELL_SIZE or self.y < 0 or self.y > SCREEN_HEIGHT - CELL_SIZE:
+            return True
+        for i in range(1, len(self.body)):
+            if self.body[i] == (self.x, self.y):
+                return True
+        return False
+
+    # 绘制贪吃蛇
+    def draw(self):
+        for x, y in self.body:
+            pygame.draw.rect(screen, self.color, [x, y, CELL_SIZE, CELL_SIZE])
+
+# 定义食物类
+class Food:
+    def __init__(self):
+        # 随机位置
+        self.x = random.randrange(0, SCREEN_WIDTH // CELL_SIZE) * CELL_SIZE
+        self.y = random.randrange(0, SCREEN_HEIGHT // CELL_SIZE) * CELL_SIZE
